1d51e89b7ef639aa5d643e90926d4d1eaa065b33
Maxim Fedorenko <m.fedorenko@tut.by>
2017-08-28 16:54:39 +0300

    Устранен крах приложения при импорте номенклатуры, в связи с попыткой изменения прогресс-бара из потока (нужно было использовать сигналы-слоты).

--- import/nomenclatureimport.h
+++ import/nomenclatureimport.h
@@ -1,7 +1,6 @@
 #ifndef NOMENCLATUREIMPORT_H
 #define NOMENCLATUREIMPORT_H
 
-#include <qprogressbar.h>
 #include <qsqlquery.h>
 #include <qstring.h>
 #include <qthread.h>
@@ -10,11 +9,11 @@ class NomenclatureImport : public QThread
 {
     Q_OBJECT
 public:
-    NomenclatureImport(QString &csvFilePath, QProgressBar &progressBar,
-                       int &mid, int &rowCount, const int &startRow, const int &articleCol,
-                       const int &nomenclatureCol, const int &unitCol, QObject *parent = 0);
+    NomenclatureImport(QString &csvFilePath, int &mid, int &rowCount,
+                       const int &startRow, const int &articleCol,
+                       const int &nomenclatureCol, const int &unitCol,
+                       QObject *parent = 0);
 private:
-    QProgressBar *progressBar;
     QString csvFilePath;
     int mid, rowCount, startRow, articleCol, nomenclatureCol,  unitCol;
     int max(int i, int j, int k);
@@ -27,6 +26,8 @@ private:
     // QThread interface
 protected:
     void run() override;
+signals:
+    int progressChanged(int);
 };
 
 #endif // NOMENCLATUREIMPORT_H
