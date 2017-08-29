53557a28b1362ad410ea1c1dd77d972f4ac6d8eb
Maxim Fedorenko <m.fedorenko@tut.by>
2017-08-29 17:46:18 +0300

    sql-load-infile in progress

---
+++ import/nomenclaturetxtfile.h
@@ -0,0 +1,22 @@
+#ifndef NOMENCLATUREINFILE_H
+#define NOMENCLATUREINFILE_H
+
+#include <qfile.h>
+#include <qtextstream.h>
+
+
+class NomenclatureTxtFile
+{
+public:
+    NomenclatureTxtFile(const QFile &source);
+    void appendNomenclatureRow(const QString &row);
+    void appendMultiplicyRow(const QString &row);
+    void closeFiles();
+private:
+    QFile nomenclatureFile;
+    QFile multiplicyFile;
+    QTextStream *nomOut;
+    QTextStream *multOut;
+};
+
+#endif // NOMENCLATUREINFILE_H
